c6271861dccbd96d3a16074723a570320d77d508
Teodoro Hochfarber <teohoch2@gmail.com>
2015-01-15 16:11:13 -0300

    the basic xml checkers are ready

--- xmlverification.py
+++ xmlverification.py
@@ -1,5 +1,10 @@
 import xml.parsers.expat
 import subprocess
+from lxml import etree
+
+from os.path import join
+
+SCHEMA_PATH = 'xmlschemas'
 
 def parsefile(filename):
 	parser = xml.parsers.expat.ParserCreate()
@@ -13,8 +18,25 @@ def xml_well_formed(filename):
 	except Exception, e:
 		return False
 
-def correspond_to_device(filename):
+def correspond_to_device(filename, device):
+	try:
+		subprocess.check_output(['grep','-c', device, filename])
+		return True
+	except subprocess.CalledProcessError:
+		return False
 
-	output = subprocess.check_output(['grep', 'py2.py', '-i', 'test.txt'])
-	return False
+def validate_scheme(filename, device):
+	f = open(join(SCHEMA_PATH, (device.lower()+'.xsd')))
+	schema_doc = etree.parse(f)
+	schema = etree.XMLSchema(schema_doc)
 
+	d = open(filename)
+	file_doc = etree.parse(d)
+
+	return schema.validate(file_doc)
+
+path = 'xmlTest'
+
+print(xml_well_formed(join(path, 'ColdCart3-17.xml')))
+print correspond_to_device(join(path, 'ColdCart3-17.xml'), 'ColdCart3')
+print validate_scheme(join(path, 'ColdCart3-17.xml'), 'dtx')
