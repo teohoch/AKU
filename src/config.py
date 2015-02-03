ff42cda59b63d44b00b5b1106dae4c659d4cd926
Teodoro Hochfarber <teohoch2@gmail.com>
2015-02-03 18:15:52 -0300

    little improvement 1

--- src/config.py
+++ src/config.py
@@ -1,10 +1,13 @@
 import ConfigParser
+import os
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
-CONFIGURATION_PATH = '/home/teohoch/PycharmProjects/AKU/Configuration/conf.ini'
+CONFIGURATION_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../Configuration/conf.ini'))
 
 config = ConfigParser.ConfigParser()
 config.read(CONFIGURATION_PATH)
 UPLOAD_FOLDER = config.get('Locations', 'Uploads')
+
 
 
